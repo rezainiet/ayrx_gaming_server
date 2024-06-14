374fac272acd5b57ee0add31c9e52adafe7ef7d5
rezainiet <masudrezaog@gmail.com>
2024-06-15 01:19:23 +0600

    fixed bookingRoutes

--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,11 +1,12 @@
 import express from "express";
 import isAuthenticated from "../middleware/isAuthenticated.js";
-import { createAppointment, checkAvailability } from "../controllers/appointmentController.js";
+import { getBookings, updateBooking, updateBookingConfirm } from "../controllers/bookingController.js";
 
 const router = express.Router();
 
-// https://www.api.onlyhumanity.co.uk/api/v1/appointment/*
-router.route('/createAppointment').post(isAuthenticated, createAppointment);
-router.route('/checkAvailability').post(isAuthenticated, checkAvailability);
+// http://localhost:4000//api/v1/booking/getBookings/:userId
+router.route('/getBookings/:userId').get(isAuthenticated, getBookings)
+router.route('/updateBooking/:bookingId').put(isAuthenticated, updateBooking)
+router.route('/updateBookingConfirm/:bookingId').put(isAuthenticated, updateBookingConfirm)
 
 export default router;
